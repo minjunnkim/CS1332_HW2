6630610c72be5065772f61ad3f0512305ef94a8e
Minjun Kim <ckandrew04@gmail.com>
2023-08-29 23:42:48 -0400

    initial

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,204 @@
+/**
+ * Your implementation of a non-circular DoublyLinkedList with a tail pointer.
+ *
+ * @author YOUR NAME HERE
+ * @version 1.0
+ * @userid YOUR USER ID HERE (i.e. gburdell3)
+ * @GTID YOUR GT ID HERE (i.e. 900000000)
+ *
+ * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
+ *
+ * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
+ */
+public class DoublyLinkedList<T> {
+
+    // Do not add new instance variables or modify existing ones.
+    private DoublyLinkedListNode<T> head;
+    private DoublyLinkedListNode<T> tail;
+    private int size;
+
+    // Do not add a constructor.
+
+    /**
+     * Adds the element to the specified index. Don't forget to consider whether
+     * traversing the list from the head or tail is more efficient!
+     *
+     * Must be O(1) for indices 0 and size and O(n) for all other cases.
+     *
+     * @param index the index at which to add the new element
+     * @param data  the data to add at the specified index
+     * @throws IndexOutOfBoundsException if index < 0 or index > size
+     * @throws IllegalArgumentException  if data is null
+     */
+    public void addAtIndex(int index, T data) {
+
+    }
+
+    /**
+     * Adds the element to the front of the list.
+     *
+     * Must be O(1).
+     *
+     * @param data the data to add to the front of the list
+     * @throws IllegalArgumentException if data is null
+     */
+    public void addToFront(T data) {
+
+    }
+
+    /**
+     * Adds the element to the back of the list.
+     *
+     * Must be O(1).
+     *
+     * @param data the data to add to the back of the list
+     * @throws IllegalArgumentException if data is null
+     */
+    public void addToBack(T data) {
+
+    }
+
+    /**
+     * Removes and returns the element at the specified index. Don't forget to
+     * consider whether traversing the list from the head or tail is more
+     * efficient!
+     *
+     * Must be O(1) for indices 0 and size - 1 and O(n) for all other cases.
+     *
+     * @param index the index of the element to remove
+     * @return the data formerly located at the specified index
+     * @throws IndexOutOfBoundsException if index < 0 or index >= size
+     */
+    public T removeAtIndex(int index) {
+
+    }
+
+    /**
+     * Removes and returns the first element of the list.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the front of the list
+     * @throws java.util.NoSuchElementException if the list is empty
+     */
+    public T removeFromFront() {
+
+    }
+
+    /**
+     * Removes and returns the last element of the list.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the back of the list
+     * @throws java.util.NoSuchElementException if the list is empty
+     */
+    public T removeFromBack() {
+
+    }
+
+    /**
+     * Returns the element at the specified index. Don't forget to consider
+     * whether traversing the list from the head or tail is more efficient!
+     *
+     * Must be O(1) for indices 0 and size - 1 and O(n) for all other cases.
+     *
+     * @param index the index of the element to get
+     * @return the data stored at the index in the list
+     * @throws IndexOutOfBoundsException if index < 0 or index >= size
+     */
+    public T get(int index) {
+
+    }
+
+    /**
+     * Returns whether or not the list is empty.
+     *
+     * Must be O(1).
+     *
+     * @return true if empty, false otherwise
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * Clears the list.
+     *
+     * Clears all data and resets the size.
+     *
+     * Must be O(1).
+     */
+    public void clear() {
+
+    }
+
+    /**
+     * Removes and returns the last copy of the given data from the list.
+     *
+     * Do not return the same data that was passed in. Return the data that
+     * was stored in the list.
+     *
+     * Must be O(1) if data is in the tail and O(n) for all other cases.
+     *
+     * @param data the data to be removed from the list
+     * @return the data that was removed
+     * @throws IllegalArgumentException if data is null
+     * @throws java.util.NoSuchElementException   if data is not found
+     */
+    public T removeLastOccurrence(T data) {
+
+    }
+
+    /**
+     * Returns an array representation of the linked list. If the list is
+     * size 0, return an empty array.
+     *
+     * Must be O(n) for all cases.
+     *
+     * @return an array of length size holding all of the objects in the
+     * list in the same order
+     */
+    public Object[] toArray() {
+
+    }
+
+    /**
+     * Returns the head node of the list.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the head of the list
+     */
+    public DoublyLinkedListNode<T> getHead() {
+        // DO NOT MODIFY!
+        return head;
+    }
+
+    /**
+     * Returns the tail node of the list.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the tail of the list
+     */
+    public DoublyLinkedListNode<T> getTail() {
+        // DO NOT MODIFY!
+        return tail;
+    }
+
+    /**
+     * Returns the size of the list.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the list
+     */
+    public int size() {
+        // DO NOT MODIFY!
+        return size;
+    }
+}
